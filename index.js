4d970c02de6a4d91336715fd887e1382738c45f6
Ali Soleimani <ali.roozbahani1994@gmail.com>
2021-10-03 17:37:55 +0330

    structure project

--- index.js
+++ index.js
@@ -1,101 +1,30 @@
-const Joi = require('joi');
+// Define moudles
+const startupDebugger = require('debug')('app:startup'),
+    myDebugger = require('debug')('app:another');
+const logger = require('./middlewares/logger');
+const config = require('config');
+const authenticator = require('./middlewares/authenticator');
 const express = require('express');
 const app = express();
+const coursesRouter = require('./routes/courses');
+const homeRouter = require('./routes/home');
 
+// Use middlewares
 app.use(express.json());
-
-const courses = [
-    { id: 1, name: 'course 1' },
-    { id: 2, name: 'course 2' },
-    { id: 3, name: 'course 3' },
-    { id: 4, name: 'course 4' },
-    { id: 5, name: 'course 5' }
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!!!');
-});
-
-app.get('/api/courses', (req, res) => {
-    if (Object.keys(req.query).length === 0) {
-        res.send(courses);
-    } else {
-        res.send(req.query);
-    }
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) {
-        return res.status(404).send("There is no course with the given id");
-    }
-    res.send(course);
-});
-
-/**
- * Creates new user
- */
-app.post('/api/courses', (req, res) => {
-
-    // Validate request body via shema
-    const { error, value } = validateCourse(req.body);
-
-    // Check error
-    if (error) {
-        return res.send(error.details[0].message);
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: value.name
-    };
-
-    courses.push(course);
-
-    res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-    // Check existence
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) {
-        return res.status(404).send("There is no course with the given id");
-    }
-
-    // Validate request body via shema
-    const { error, value } = validateCourse(req.body);
-
-    // Check error
-    if (error) {
-        return res.send(error.details[0].message);
-    }
-
-    course.name = value.name;
-    res.send(course);
-
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) {
-        return res.status(404).send("The course with the given ID not found.");
-    }
-
-    courses.splice(courses.indexOf(course), 1);
-
-    res.send("Successfully deleted");
-});
-
-function validateCourse(requestBody) {
-    // Define a shema for validating inputs came from client
-    const schema = Joi.object({
-        name: Joi.string()
-            .min(3)
-            .required()
-    });
-
-    return schema.validate(requestBody);
+if (app.get('env') === 'development') {
+    app.use(logger);
+    startupDebugger('"Logger is enabled"');
 }
+app.use(authenticator);
 
+// Routers
+app.use('/', homeRouter);
+app.use('/api/courses', coursesRouter);
+
+// Launch the app
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to the port ${port}...`));
+
+myDebugger(config.get('name'));
+myDebugger(config.get('mail.host'));
+myDebugger(config.get('mail.password'));
---
+++ routes/courses.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+
+
+const courses = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' },
+    { id: 4, name: 'course 4' },
+    { id: 5, name: 'course 5' }
+];
+
+router.get('/', (req, res) => {
+    if (Object.keys(req.query).length === 0) {
+        res.send(courses);
+    } else {
+        res.send(req.query);
+    }
+});
+
+router.get('/:id', (req, res) => {
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send("There is no course with the given id");
+    }
+    res.send(course);
+});
+
+
+router.post('/', (req, res) => {
+
+    // Validate request body via shema
+    const { error, value } = validateCourse(req.body);
+
+    // Check error
+    if (error) {
+        return res.send(error.details[0].message);
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: value.name
+    };
+
+    courses.push(course);
+
+    res.send(course);
+});
+
+router.put('/:id', (req, res) => {
+    // Check existence
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send("There is no course with the given id");
+    }
+
+    // Validate request body via shema
+    const { error, value } = validateCourse(req.body);
+
+    // Check error
+    if (error) {
+        return res.send(error.details[0].message);
+    }
+
+    course.name = value.name;
+    res.send(course);
+
+});
+
+router.delete('/:id', (req, res) => {
+    const course = courses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send("The course with the given ID not found.");
+    }
+
+    courses.splice(courses.indexOf(course), 1);
+
+    res.send("Successfully deleted");
+});
+
+function validateCourse(requestBody) {
+    // Define a shema for validating inputs came from client
+    const schema = Joi.object({
+        name: Joi.string()
+            .min(3)
+            .required()
+    });
+
+    return schema.validate(requestBody);
+}
+
+module.exports = router;
