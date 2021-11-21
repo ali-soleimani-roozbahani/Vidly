cd3e9111bca6a57e9dcb394d7f0b96cd469aed10
ali.soleimani <ali.soleimanimysso1@partdp.ir>
2021-11-21 16:48:12 +0330

    Close server in beforeEach block

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -10,7 +10,7 @@ describe('/api/genres', () => {
 
     afterEach(async () => {
         await Genre.remove({});
-        server.close();
+        await server.close();
     });
 
     describe('GET /', () => {
